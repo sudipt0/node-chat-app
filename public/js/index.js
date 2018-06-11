aa021d2716e4394375ac383ffde1ab7ed6eab7eb
Sudipta Kumar Mondal <sudipta@Sudipta.local>
2018-06-12 00:47:06 +0530

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,10 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server');
 
+    // socket.emit('createMessage', {
+    //     from: 'josh@example.com',
+    //     text: 'Hello! how are you',
+    // })
 });
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
